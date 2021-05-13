9e9a93c0d30b92e5a0e7b360966b85552e24d143
sedx876 <swatkins876@gmail.com>
2021-05-13 06:55:10 -0400

    Added MailContentBuilder service

--- reddit/src/main/java/com/sw/reddit/service/AuthService.java
+++ reddit/src/main/java/com/sw/reddit/service/AuthService.java
@@ -2,13 +2,16 @@ package com.sw.reddit.service;
 
 import com.sw.reddit.dto.RegisterRequest;
 import com.sw.reddit.model.User;
+import com.sw.reddit.model.VerificationToken;
 import com.sw.reddit.repository.UserRepository;
+import com.sw.reddit.repository.VerificationTokenRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +20,7 @@ public class AuthService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final VerificationTokenRepository verificationTokenRepository;
 
   public void signup(RegisterRequest registerRequest){
     User user = new User();
@@ -27,5 +31,16 @@ public class AuthService {
     user.setEnabled(false);
 
     userRepository.save(user);
+    String token = generateVerificationToken(user);
+  }
+
+  private String generateVerificationToken(User user) {
+    String token = UUID.randomUUID().toString();
+    VerificationToken verificationToken = new VerificationToken();
+    verificationToken.setToken(token);
+    verificationToken.setUser(user);
+
+    verificationTokenRepository.save(verificationToken);
+    return token;
   }
 }
