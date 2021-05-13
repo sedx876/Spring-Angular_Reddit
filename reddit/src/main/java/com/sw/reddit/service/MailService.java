e002807942b3a15e16d84d3b7252997f1b18efc2
sedx876 <swatkins876@gmail.com>
2021-05-13 07:16:29 -0400

    Added SpringRedditException

--- reddit/src/main/java/com/sw/reddit/service/MailService.java
+++ reddit/src/main/java/com/sw/reddit/service/MailService.java
@@ -1,14 +1,15 @@
 package com.sw.reddit.service;
 
+import com.sw.reddit.exceptions.SpringRedditException;
 import com.sw.reddit.model.NotificationEmail;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
